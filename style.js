edc035771c9ad247e786b5298ea588e0ddc7ef66
AparisBlink <61824533+AparisBlink@users.noreply.github.com>
2020-03-15 20:43:28 +0100

    Update style.js

--- style.js
+++ style.js
@@ -17,6 +17,8 @@
 				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title bck-title1'} },
 				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title bck-title2'} },
 				{ name: 'Título 3', element: 'h4', attributes: { 'class': 'bck-title bck-title3'} },
+				{ name: 'Título 4', element: 'h4', attributes: { 'class': 'bck-title bck-title4'} },
+				{ name: 'Título 5', element: 'h4', attributes: { 'class': 'bck-title bck-title5'} },
 
 				{ name: 'Lista ordenada 1 Verde', element: 'ol', attributes: { 'class': 'bck-ol bck-ol1' } },
 				{ name: 'Lista ordenada 2 Azul', element: 'ol', attributes: { 'class': 'bck-ol bck-ol2' } },
